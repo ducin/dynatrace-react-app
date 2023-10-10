34a1ec22fdc0f182f25ee5a524bf3cae3fe0d138
Tomasz Ducin <tomasz@ducin.it>
2023-10-10 23:12:29 +0200

    chore: initial Dynatrace App

---
+++ app.config.ts
@@ -0,0 +1,14 @@
+import type { CliOptions } from 'dt-app';
+
+const config: CliOptions = {
+  environmentUrl: 'https://hwv77047.apps.dynatrace.com/',
+  app: {
+    name: 'dt-playground',
+    version: '0.0.0',
+    description: 'A starting project with routing, fetching data, and charting',
+    id: 'my.dt.playground',
+    scopes: [{ name: 'storage:logs:read', comment: 'default template' }, { name: 'storage:buckets:read', comment: 'default template' }]
+  },
+};
+
+module.exports = config;
